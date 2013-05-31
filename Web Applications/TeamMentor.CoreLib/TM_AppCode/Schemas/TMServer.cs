743855ddc45d76e767cf98dbfa715d94b3e7fc03
Dinis Cruz <dinis.cruz@owasp.org>
2013-05-31 11:48:58 +0100

    Added TMServer.cs (to hold server configs) and moved (+renamed) the schema extension method files into a separate folder (using the 'ExtMet' tag in name)

---
+++ Web Applications/TeamMentor.CoreLib/TM_AppCode/Schemas/TMServer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamMentor.CoreLib
+{
+    public class TMServer
+    {
+        public List<TMServer_UserDataRepo> UserDataRepos { get; set; }
+
+        public TMServer()
+        {
+            UserDataRepos = new List<TMServer_UserDataRepo>();
+        }
+
+        public class TMServer_UserDataRepo
+        {
+            public string Name  { get; set; }
+            public string Path  { get; set; }
+            public bool Enabled { get; set; }
+        }
+    }
+
+    
+}
