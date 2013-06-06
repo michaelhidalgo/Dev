bf881692f59a51b19a5c2342cc2a04543f31eccd
Dinis Cruz <dinis.cruz@owasp.org>
2013-06-06 10:29:48 +0100

    Adding ASP.NET MVC4 references (from NuGet) and added new MVC4.cs Util class (which will do the Route Setup)

---
+++ Web Applications/TeamMentor.CoreLib/TM_AppCode/Utils/MVC4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TeamMentor.CoreLib.TM_AppCode.Utils
+{
+    public class MVC4
+    {
+        public MVC4()
+        {
+            SetUp_ASPNET_MVC();
+        }
+
+        public void SetUp_ASPNET_MVC()
+        {
+            //AreaRegistration.RegisterAllAreas();
+            RouteTable.Routes.MapRoute("Default",
+                                                   "{controller}/{action}/{id}",
+                                                   new { action = "FirstAction", id = UrlParameter.Optional });
+        }
+
+
+    }
+}
