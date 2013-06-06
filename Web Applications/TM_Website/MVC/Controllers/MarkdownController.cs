f388d76e0a5176d4914eb090e8fdc8e2a978f51a
Dinis Cruz <dinis.cruz@owasp.org>
2013-06-06 09:46:33 +0100

    Adding Loggin_Needed.cshtml page and some role based detection on the Markdown controllers Removed the MVC route configuration from MVC_Config.cs (since that will be moved into TeamMentor.CoreLib.dll)

--- Web Applications/TM_Website/MVC/Controllers/MarkdownController.cs
+++ Web Applications/TM_Website/MVC/Controllers/MarkdownController.cs
@@ -23,6 +23,8 @@ namespace TeamMentor.Website
         //view article
         public ActionResult Viewer(string articleId)
         {
+            if (UserRole.ReadArticles.currentUserHasRole().isFalse())
+                return View(@"~/MVC/Views/Loggin_Needed.cshtml");
             var markdownToRender = getArticle(articleId).Content.Data_Json;
             ViewData["Content"] = markdownToRender.markdown_transform();
             ViewData["ArticleId"] = articleId;
@@ -32,6 +34,9 @@ namespace TeamMentor.Website
         //show article editor        
         public ActionResult Editor(string articleId)
         {
+            if (UserRole.EditArticles.currentUserHasRole().isFalse())
+                return View(@"~/MVC/Views/Loggin_Needed.cshtml");
+
             var article = getArticle(articleId);
             if (article.notNull())
                 ViewData["Content"] = getArticle(articleId).Content.Data_Json;
